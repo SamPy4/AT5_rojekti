31cd3f421d11fc0ebbb2605b2e34a6521a316448
SamPy4 <sami.porio@gmail.com>
2017-12-19 08:35:34 +0200

    First commit

---
+++ sivu.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>LOGIN</title>
+  <!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
+</head>
+<body>
+  <div id="frm">
+    <!-- <form action="process.php" method="POST"> -->
+      <p>
+        <label>Käyttäjä:</label>
+        <input type="text" id="user" name="user" />
+      </p>
+      <p>
+        <label>Salasana:</label>
+        <input type="password" id="pass" name="pass" />
+      </p>
+      <p>
+        <input type="submit" id="btn" name="Login" />
+      </p>
+  </div>
+
+</body>
+</html>
