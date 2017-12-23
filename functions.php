be92b6b31a4adf34258bccf59184b38329c47bc7
SamPy4 <sami.porio@gmail.com>
2017-12-23 22:34:51 +0200

    More reliable

--- functions.php
+++ functions.php
@@ -28,6 +28,28 @@
       $pvm_to = date('Y-m-d');
       $pvm_pe = date('Y-m-d', strtotime(' +1 day'));
     }
+    elseif (date('N') == 5) {
+      $pvm_ma = date('Y-m-d', strtotime(' -3 day'));
+      $pvm_ti = date('Y-m-d', strtotime(' -2 day'));
+      $pvm_ke = date('Y-m-d', strtotime(' -1 day'));
+      $pvm_to = date('Y-m-d', strtotime(' +1 day'));
+      $pvm_pe = date('Y-m-d');
+    }
+    elseif (date('N') == 6) {
+      $pvm_ma = date('Y-m-d', strtotime(' +2 day'));
+      $pvm_ti = date('Y-m-d', strtotime(' +3 day'));
+      $pvm_ke = date('Y-m-d', strtotime(' +4 day'));
+      $pvm_to = date('Y-m-d', strtotime(' +5 day'));
+      $pvm_pe = date('Y-m-d', strtotime(' +6 day'));
+    }
+    elseif (date('N') == 6) {
+      $pvm_ma = date('Y-m-d', strtotime(' +1 day'));
+      $pvm_ti = date('Y-m-d', strtotime(' +2 day'));
+      $pvm_ke = date('Y-m-d', strtotime(' +3 day'));
+      $pvm_to = date('Y-m-d', strtotime(' +4 day'));
+      $pvm_pe = date('Y-m-d', strtotime(' +5 day'));
+    }
+    
     $pvms = array($pvm_ma, $pvm_ti, $pvm_ke, $pvm_to, $pvm_pe);
     return $pvms;
   }
