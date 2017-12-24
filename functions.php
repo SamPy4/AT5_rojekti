9c81a5705afa72d342c0193e7e23ea4accd0c772
SamPy4 <sami.porio@gmail.com>
2017-12-24 19:12:14 +0200

    Now it works in sundays too

--- functions.php
+++ functions.php
@@ -42,14 +42,14 @@
       $pvm_to = date('Y-m-d', strtotime(' +5 day'));
       $pvm_pe = date('Y-m-d', strtotime(' +6 day'));
     }
-    elseif (date('N') == 6) {
+    elseif (date('N') == 7) {
       $pvm_ma = date('Y-m-d', strtotime(' +1 day'));
       $pvm_ti = date('Y-m-d', strtotime(' +2 day'));
       $pvm_ke = date('Y-m-d', strtotime(' +3 day'));
       $pvm_to = date('Y-m-d', strtotime(' +4 day'));
       $pvm_pe = date('Y-m-d', strtotime(' +5 day'));
     }
-    
+
     $pvms = array($pvm_ma, $pvm_ti, $pvm_ke, $pvm_to, $pvm_pe);
     return $pvms;
   }
