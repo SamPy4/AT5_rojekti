fcc70f72934ef551d17de41c4943a03f9c3f9bc0
SamPy4 <sami.porio@gmail.com>
2017-12-25 23:14:20 +0200

    Working parsing protocol. Simple next,prev buttons added. Database filled.

--- main.php
+++ main.php
@@ -2,12 +2,36 @@
   ini_set('display_errors', 1);
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
+
   include 'functions.php';
   include 'mySQL_func.php';
+
 
   $date_today = date("Y-m-d");
 
   list($pvm_ma, $pvm_ti, $pvm_ke, $pvm_to, $pvm_pe) = def_days();  // Luo viikonpäivä muuttujille päivämäärät
+
+  $pvm_ma = '2018-01-01';
+  $pvm_ti = '2018-01-02';
+  $pvm_ke = '2018-01-03';
+  $pvm_to = '2018-01-04';
+  $pvm_pe = '2018-01-05';
+
+  if(isset($_POST['pvm_ma'])) {
+    $pvm_ma = $_POST['pvm_ma'];
+  }
+  if(isset($_POST['pvm_ti'])) {
+    $pvm_ti = $_POST['pvm_ti'];
+  }
+  if(isset($_POST['pvm_ke'])) {
+    $pvm_ke = $_POST['pvm_ke'];
+  }
+  if(isset($_POST['pvm_to'])) {
+    $pvm_to = $_POST['pvm_to'];
+  }
+  if(isset($_POST['pvm_pe'])) {
+    $pvm_pe = $_POST['pvm_pe'];
+  }
 
   list($ma_ruoka, $ti_ruoka, $ke_ruoka, $to_ruoka, $pe_ruoka) = get_ruoat($pvm_ma, $pvm_ti, $pvm_ke, $pvm_to, $pvm_pe);
 
@@ -88,6 +112,30 @@
      <p5>
        <?php echo $pvm_pe ?>
      </p5>
+   </div>
+
+   <div>
+     <form action="main.php" method="post" id="next_week">
+         <input type="hidden" name="pvm_ma" value="<?php echo date('Y-m-d', strtotime($pvm_ma. ' + 7 days')) ?> "/>
+         <input type="hidden" name="pvm_ti" value="<?php echo date('Y-m-d', strtotime($pvm_ti. ' + 7 days')) ?> "/>
+         <input type="hidden" name="pvm_ke" value="<?php echo date('Y-m-d', strtotime($pvm_ke. ' + 7 days')) ?> "/>
+         <input type="hidden" name="pvm_to" value="<?php echo date('Y-m-d', strtotime($pvm_to. ' + 7 days')) ?> "/>
+         <input type="hidden" name="pvm_pe" value="<?php echo date('Y-m-d', strtotime($pvm_pe. ' + 7 days')) ?> "/>
+
+         <input type="submit" value="Seuraava viikko" />
+     </form>
+   </div>
+
+   <div>
+     <form action="main.php" method="post" id="prev_week">
+         <input type="hidden" name="pvm_ma" value="<?php echo date('Y-m-d', strtotime($pvm_ma. ' - 7 days')) ?> "/>
+         <input type="hidden" name="pvm_ti" value="<?php echo date('Y-m-d', strtotime($pvm_ti. ' - 7 days')) ?> "/>
+         <input type="hidden" name="pvm_ke" value="<?php echo date('Y-m-d', strtotime($pvm_ke. ' - 7 days')) ?> "/>
+         <input type="hidden" name="pvm_to" value="<?php echo date('Y-m-d', strtotime($pvm_to. ' - 7 days')) ?> "/>
+         <input type="hidden" name="pvm_pe" value="<?php echo date('Y-m-d', strtotime($pvm_pe. ' - 7 days')) ?> "/>
+
+         <input type="submit" name="laheta" value="Edellinen viikko" />
+     </form>
    </div>
 
  </body>
