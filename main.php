5e5413a4d24b2569da1f323501ffb13ea9c9e00c
SamPy4 <sami.porio@gmail.com>
2017-12-26 23:08:18 +0200

    Ihan saa**nasti muutoksia.

--- main.php
+++ main.php
@@ -11,11 +11,11 @@
 
   list($pvm_ma, $pvm_ti, $pvm_ke, $pvm_to, $pvm_pe) = def_days();  // Luo viikonpäivä muuttujille päivämäärät
 
-  $pvm_ma = '2018-01-01';
-  $pvm_ti = '2018-01-02';
-  $pvm_ke = '2018-01-03';
-  $pvm_to = '2018-01-04';
-  $pvm_pe = '2018-01-05';
+  // $pvm_ma = '2018-01-01';
+  // $pvm_ti = '2018-01-02';
+  // $pvm_ke = '2018-01-03';
+  // $pvm_to = '2018-01-04';
+  // $pvm_pe = '2018-01-05';
 
   if(isset($_POST['pvm_ma'])) {
     $pvm_ma = $_POST['pvm_ma'];
@@ -33,6 +33,52 @@
     $pvm_pe = $_POST['pvm_pe'];
   }
 
+  $today  = new DateTime();
+  $today->format('Y\\-m\\-d\\');
+
+  $date_b = new DateTime($pvm_ke);
+
+  // if (DateTime::createFromFormat("Y-m-d", date("Y-m-d")) == DateTime::createFromFormat("Y-m-d", "2017-12-26")) {
+  //   echo "NYT ON kyl";
+  // }
+
+
+
+
+  // Kokeillaan mikä päivä on ja asetetaan se vihreäksi. Menneet päivät harmaiksi ja tulevvat pysyvät ennallaan
+  
+  // $today = date("Y-m-d", strtotime("2017-12-26"));
+  $today = date("Y-m-d");
+  if (date("Y-m-d", strtotime($pvm_ma)) == $today) {
+    $what_style = "style_ma.css";
+  }
+  elseif (date("Y-m-d", strtotime($pvm_ti)) == $today) {
+    $what_style = "style_ti.css";
+  }
+  elseif (date("Y-m-d", strtotime($pvm_ke)) == $today) {
+    $what_style = "style_ke.css";
+  }
+  elseif (date("Y-m-d", strtotime($pvm_to)) == $today) {
+    $what_style = "style_to.css";
+  }
+  elseif (date("Y-m-d", strtotime($pvm_pe)) == $today) {
+    $what_style = "style_pe.css";
+  }
+  elseif (date("Y-m-d", strtotime($pvm_ma)) < $today) {
+    $what_style = "style_kaikki_menny.css";
+  }
+  elseif (date("Y-m-d", strtotime($pvm_ma)) > $today) {
+    $what_style = "style_kaikki_tulossa.css";
+  }
+  // echo $pvm_pe == $today;
+
+  $new_date = date_create($pvm_ma);
+  $today  = date_create(date("Y-m-d"));
+
+  $diff = date_diff($new_date, $today, True);
+  // echo $diff->format('%d');
+
+  // echo strtotime($pvm_ma) > date("Y-m-d");
   list($ma_ruoka, $ti_ruoka, $ke_ruoka, $to_ruoka, $pe_ruoka) = get_ruoat($pvm_ma, $pvm_ti, $pvm_ke, $pvm_to, $pvm_pe);
 
  ?>
@@ -97,6 +143,7 @@
    </div>
 
    <div id="pvm_texts">
+     <link rel="stylesheet" type="text/css" href= <?php echo $what_style ?>>
      <p1>
        <?php echo $pvm_ma ?>
      </p1>
@@ -113,9 +160,26 @@
        <?php echo $pvm_pe ?>
      </p5>
    </div>
+   <script>
+    document.onkeydown=function(evt){
+        var keyCode = evt ? (evt.which ? evt.which : evt.keyCode) : event.keyCode;
+        // alert(keyCode);
 
+        if(keyCode === 39) {
+            document.next_week.submit();
+        }
+
+        if(keyCode === 32) {
+            document.this_week.submit();
+        }
+
+        if(keyCode === 37) {
+            document.prev_week.submit();
+        }
+    }
+  </script>
    <div>
-     <form action="main.php" method="post" id="next_week">
+     <form name="next_week" action="main.php" method="post" id="next_week">
          <input type="hidden" name="pvm_ma" value="<?php echo date('Y-m-d', strtotime($pvm_ma. ' + 7 days')) ?> "/>
          <input type="hidden" name="pvm_ti" value="<?php echo date('Y-m-d', strtotime($pvm_ti. ' + 7 days')) ?> "/>
          <input type="hidden" name="pvm_ke" value="<?php echo date('Y-m-d', strtotime($pvm_ke. ' + 7 days')) ?> "/>
@@ -127,7 +191,20 @@
    </div>
 
    <div>
-     <form action="main.php" method="post" id="prev_week">
+     <form name="this_week" action="main.php" method="post" id="this_week">
+         <input type="hidden" name="pvm_ma" value="<?php echo def_day("ma") ?> "/>
+         <input type="hidden" name="pvm_ti" value="<?php echo def_day("ti") ?> "/>
+         <input type="hidden" name="pvm_ke" value="<?php echo def_day("ke") ?> "/>
+         <input type="hidden" name="pvm_to" value="<?php echo def_day("to") ?> "/>
+         <input type="hidden" name="pvm_pe" value="<?php echo def_day("pe") ?> "/>
+
+         <button type="submit" class="this_week_but" /><span>Tänään</span> </button>
+     </form>
+   </div>
+
+
+   <div>
+     <form name="prev_week" action="main.php" method="post" id="prev_week">
          <input type="hidden" name="pvm_ma" value="<?php echo date('Y-m-d', strtotime($pvm_ma. ' - 7 days')) ?> "/>
          <input type="hidden" name="pvm_ti" value="<?php echo date('Y-m-d', strtotime($pvm_ti. ' - 7 days')) ?> "/>
          <input type="hidden" name="pvm_ke" value="<?php echo date('Y-m-d', strtotime($pvm_ke. ' - 7 days')) ?> "/>
@@ -137,6 +214,14 @@
          <button type="submit" class="prev_week_but" /><span>Edellinen viikko</span> </button>
      </form>
    </div>
+
+   <!-- <div>
+     <form name="search" action="main.php" method="post" id="search">
+       <label id="search_text">Etsi:</label>
+       <input type="text" name="search_box" />
+       <button type="submit" class="search_but" /><span>Etsi</span> </button>
+     </form>
+   </div> -->
 
  </body>
  </html>
